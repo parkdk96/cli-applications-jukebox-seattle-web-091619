23be9525795aafa454955fb0bd89863f295f5d72
Daniel Park <parkdk96@gmail.com>
2019-08-27 06:51:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,10 +1,13 @@
 # Add your code here
 def help 
   puts "I accept the following commands:"
-  puts "- help : displays this help"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
-def play
+def play(song)
   
 end
 
