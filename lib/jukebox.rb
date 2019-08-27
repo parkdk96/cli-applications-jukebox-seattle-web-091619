9a46828cba25ec3b88968cd5eb4a37d2e3a2097d
Daniel Park <parkdk96@gmail.com>
2019-08-27 06:44:52 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,10 +1,1 @@
 # Add your code here
-def say_hello(name)
-  "Hi #{name}!"
-end
-
-puts "Enter your name:"
-#strip removes whitespace as well as \n
-users_name = gets.strip
-
-puts say_hello(users_name)
