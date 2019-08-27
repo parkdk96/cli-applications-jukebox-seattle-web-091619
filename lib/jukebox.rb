7c35bc4a25410d440006d5bd66fcddeac34d49b4
Daniel Park <parkdk96@gmail.com>
2019-08-27 07:05:49 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,14 +11,20 @@ def play(songs)
   puts "Please enter a song name or number:"
   song = gets.strip
   if songs.includes?(song)
-    
+    puts "Playing #{song}"
+  else 
+    puts "Invalid input, please try again"
 end
 
 def list (songs)
-  songs.each_with_index {|name, index| puts "#{index}. #{track} "}
-    
+  songs.each_with_index {|name, index| puts "#{index + 1}. #{track} "}
 end
 
 def exit_jukebox
   puts "Goodbye"
+end
+
+def run 
+  puts "Please enter a command:"
+  input = gets.strip
 end
